e497332cbff1bdd0125eb9394fda0dabeba9be8a
ErikMurmi <erikmurmi@gmail.com>
2023-07-19 20:13:12 -0500

    edit controls

---
+++ pages/controles/editar.js
@@ -0,0 +1,139 @@
+import { updateItem } from "controllers/modelosController";
+import { useState, useEffect } from "react";
+import { getItems } from "controllers/modelosController";
+import { useRouter } from "next/router";
+import { getItem } from "controllers/modelosController";
+
+const modelName = "controles";
+const Control = {
+  Control: "",
+  Contexto: "",
+  Discusion: "",
+  Categoria: "",
+  Riesgo: "",
+};
+
+const opcionesCategoria = [
+  "Control de Acceso",
+  "Seguridad de Red",
+  "Respaldos",
+  "Auditorías",
+];
+
+export default function CrearControl({ riesgos }) {
+  const [formulario, setFormulario] = useState(Control);
+  const router = useRouter(); // Obtener el router
+  const { id } = router.query; // Obtener el id de la URL
+
+  useEffect(() => {
+    if (id) {
+      const obtenerItem = async () => {
+        try {
+          const item = await getItem(modelName, id);
+          setFormulario(item);
+        } catch (error) {
+          console.error("Error al obtener el item:", error);
+        }
+      };
+
+      obtenerItem();
+    }
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormulario({ ...formulario, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar los datos del formulario a la base de datos o realizar cualquier otra acción.
+    //console.log(formulario);
+    const actualizado = await updateItem(formulario, modelName, id);
+    let msg = "No se pudo crear el item";
+    if (actualizado) {
+      // Restablecer el formulario después de enviar los datos
+      setFormulario(Amenaza);
+      msg = "Se creo el item correctamente";
+      alert(msg);
+      router.replace(`/${modelName}`);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Editar Control</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Control:</label>
+          <input
+            type="text"
+            name="Control"
+            value={formulario.Control}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Contexto:</label>
+          <input
+            type="text"
+            name="Contexto"
+            value={formulario.Contexto}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Discusion:</label>
+          <input
+            type="text"
+            name="Discusion"
+            value={formulario.Discusion}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Categoria:</label>
+          <select
+            name="Categoria"
+            value={formulario.Categoria}
+            onChange={handleChange}
+          >
+            <option value="">Selecciona una Categoria</option>
+            {opcionesCategoria.map((categoria, index) => (
+              <option key={index} value={categoria}>
+                {categoria}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Riesgo:</label>
+          <select
+            name="Riesgo"
+            value={formulario.Riesgo}
+            onChange={handleChange}
+          >
+            <option value="">Selecciona un Riesgo</option>
+            {riesgos.map((riesgo) => (
+              <option key={riesgo.Nombre} value={riesgo.Nombre}>
+                {riesgo.Nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="submit-btn" type="submit">
+          Editar Control
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const riesgos = await getItems("riesgos");
+  return {
+    props: {
+      riesgos: riesgos,
+    },
+  };
+};
--- pages/controles/index.js
+++ pages/controles/index.js
@@ -5,18 +5,10 @@ import { useRouter } from "next/router";
 const modelName = "controles";
 
 export default function Controles({ controles }) {
-  // const controles = [
-  //   {
-  //     Control: "Antivirus",
-  //     Contexto: "Protección contra malware",
-  //     Discusion: "Uso de antivirus actualizado en todos los dispositivos",
-  //     Categoria: "Seguridad Informática",
-  //     Riesgo: "Virus",
-  //   },
-  //   // Agrega más objetos control aquí...
-  // ];
   const router = useRouter();
-
+  function handleEdit(id) {
+    router.push(`${modelName}/editar?id=${id}`);
+  }
   return (
     <>
       <h1>Tabla de Controles</h1>
@@ -27,7 +19,12 @@ export default function Controles({ controles }) {
       >
         Agregar
       </button>
-      <ModelTable model={Control} data={controles} modelName={modelName} />
+      <ModelTable
+        model={Control}
+        data={controles}
+        modelName={modelName}
+        editFun={handleEdit}
+      />
     </>
   );
 }
